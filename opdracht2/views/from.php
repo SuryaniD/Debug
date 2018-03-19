f7c8504e2f3a6c5a7e333b56927e68bddb3b71d9
SuryaniD <34694707+SuryaniD@users.noreply.github.com>
2018-03-19 08:32:09 +0100

    Add files via upload

---
+++ opdracht2/views/from.php
@@ -0,0 +1,16 @@
+<h1>Contactformulier</h1>
+<h3>zet hier jouw naam en klas</h3>
+<hr>
+<pre>
+  <form method="post" action="views/response.php">
+
+    naam: 	 <input type="text" name="name" autofocus>
+
+    email: 	  <input type="email" name="e-mail">
+
+    bericht: 	<input type="text" name="message">
+
+    			    <input type="submit" name='sendContact'>
+  </form>
+</pre>
+<hr>
